57836e12c98148a01029edee5929ff1f20844060
Maksym Skubiak <skubiakcreates@gmail.com>
2025-03-22 16:17:37 +0100

    commit

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -2,22 +2,22 @@ import css from "./TransactionHistory.module.css";
 function TransactionHistory({ items }) {
   return (
     <>
-      <table>
+      <table className={css.table}>
         <thead>
           <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
+            <th className={css.th}>Type</th>
+            <th className={css.th}>Amount</th>
+            <th className={css.th}>Currency</th>
           </tr>
         </thead>
 
         <tbody>
           {items.map(({ id, type, amount, currency }) => {
             return (
-              <tr key={id}>
-                <td>{type}</td>
-                <td>{amount}</td>
-                <td>{currency}</td>
+              <tr key={id} className={css.tr}>
+                <td className={css.td}>{type}</td>
+                <td className={css.td}>{amount}</td>
+                <td className={css.td}>{currency}</td>
               </tr>
             );
           })}
